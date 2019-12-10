4201ed73ae37080ff00032e0ec10bd9303c9a851
Teo <ted.krastev@gmail.com>
2019-12-10 18:32:00 -0500

    added sales processing a sale

--- statements.sql
+++ statements.sql
@@ -1,12 +1,10 @@
-/* number 1 */
+/* PHOTO APPLICATIONS */
 INSERT INTO Photo ( PhotoId, Speed, Film, Fstop, Color, Resolution, 
 		    Price, `Date`, PName, PBDate )
        VALUES ( 57, 10, 35, 10.2, 'Yes', '640x480', 100, now(), 'Will Smith', 9-25-1968 
 );
-/* number 2 */
 DELETE FROM Photo WHERE PhotoId=57;
 
-/* number 3 */
 UPDATE Photo 
 SET
 	Speed = 11,
@@ -19,9 +17,22 @@ SET
 WHERE
 	PhotoID=57;
 
-/* number 4 */
 SELECT * FROM PHOTO WHERE PhotoID=57;
 
-/* number 5 */
+/* SALES APPLICATIONS */
+
+INSERT INTO Customer ( LoginName, Password, CName, CType, CardNo, BillingAddress, Str1, City, State, Zip )
+       VALUES ( 'jimmy3', '123', 'James', 'Blunt', 'Mastercard', 1111111111111111, '23 willow st, Newark, NJ, 07101', 'Willow', 'Newark', 'NJ', 07041 ) 
+
+INSERT INTO Transaction ( Tdate, CardNo, CardType, CardExpDate, TotalAmount )
+       VALUES ( now(), 1111111111111111, 'Mastercard', 11-5-2020, 105.3 ) 
+
+UPDATE Photo, Transaction
+SET
+	Photo.Sold = 'Y'
+	Photo.TransID = Transaction.TransID
+WHERE 
+	PhotoID=57;
+
 
 
