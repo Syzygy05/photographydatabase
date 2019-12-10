6c7e06e2a4e8004ef3f1f2d8351265d9967fafa0
Teo <ted.krastev@gmail.com>
2019-12-10 16:43:30 -0500

    first insert statement done, changed film from varchar to int

---
+++ statements.sql
@@ -0,0 +1,6 @@
+INSERT INTO Photo ( PhotoId, Speed, Film, Fstop, Color, Resolution, 
+		    Price, `Date`, PName, PBDate )
+       VALUES ( 57, 10, 35, 10.2, 'Yes', '640x480', 100, now(), 'Will Smith', 9-25-1968 
+);
+
+
