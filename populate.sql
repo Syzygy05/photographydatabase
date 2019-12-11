cf223d10655a23f05ef70ed17ee40c889b3f44e2
Teo <ted.krastev@gmail.com>
2019-12-10 20:04:31 -0500

    changed date format

--- populate.sql
+++ populate.sql
@@ -2,7 +2,7 @@
 INSERT INTO Customer ( LoginName, Password, CName, CType, BillingAddress, Str1, City, State, Zip )
        VALUES ( 'jimmy3', '123', 'James Blunt', 'Mastercard', '23 willow st, Newark, NJ, 07101', 'Willow', 'Newark', 'NJ', 07101 )
 
-INSERT INTO Customer ( LoginName, Password, CName, CType, BillingAddress, Str1, City, State, Zip )$
+INSERT INTO Customer ( LoginName, Password, CName, CType, BillingAddress, Str1, City, State, Zip )
        VALUES ( 'johnny12', '321', 'Johnny Bravo', 'Mastercard', '23 willow st, Newark, NJ, 07101', 'Willow', 'Newark', 'NJ', 07101 )
 
 INSERT INTO Customer ( LoginName, Password, CName, CType, BillingAddress, Str1, City, State, Zip )
@@ -20,31 +20,31 @@ INSERT INTO Photographer (
 
 INSERT INTO Photographer (
 	PName, PBDate, PBio, PAddress, PNationality ) VALUES (
-	'Martin', 7-5-1975, 'cooler than jeff', '32 wywietrnzik avenue', 'Egyptian' );
+	'Martin', "1975-7-5", 'cooler than jeff', '32 wywietrnzik avenue', 'Egyptian' );
 
 
 INSERT INTO Model (
 	MName, MBDate, MBio, MSex ) Values (
-	'Tommy', 12-18-1951, 'straight up gangster', 'M' );
+	'Tommy', "1951-12-18", 'straight up gangster', 'M' );
 
 
 INSERT INTO Model (
 	MName, MBDate, MBio, MSex ) Values (
-	'Dave', 12-18-1951, 'really wanted to be an engineer instead', 'M' );
+	'Dave', "1953-12-3", 'really wanted to be an engineer instead', 'M' );
 
 
 INSERT INTO Model (
 	MName, MBDate, MBio, MSex ) Values (
-	'Sarah OConnor', 12-18-1951, 'cool girl', 'F' );
+	'Sarah OConnor', "1965-7-3", 'cool girl', 'F' );
 
 INSERT INTO Photo (
 	PhotoID, Speed, Film FStop, Color, Resolution, Price, `Date`, PName, PBDate ) Values (
-	33, 10, 12.3, 'YES', '640x480', 100.23, 6-6-1966, 'Sally', '6-2-1983' );
+	33, 10, 12.3, 'YES', '640x480', 100.23, ,"1966-6-6", 'Sally', '1973-6-2' );
 
 INSERT INTO Photo (
 	PhotoID, Speed, Film FStop, Color, Resolution, Price, `Date`, PName, PBDate ) Values (
-	31, 11, 15.2, 'NO', '640x480', 150.23, 1-3-1987, 'Jeff', '11-2-1998' );
+	31, 11, 15.2, 'NO', '640x480', 150.23, "1987-1-3", 'Jeff', '1998-11-2' );
 
 INSERT INTO Photo (
 	PhotoID, Speed, Film FStop, Color, Resolution, Price, `Date`, PName, PBDate ) Values (
-	30, 12, 13.2, 'yes', '640x480', 65.50, 4-6-1999, 'Tommy', '12-19-1951' );
+	30, 12, 13.2, 'yes', '640x480', 65.50, "1999-4-6", 'Tommy', "1951-12-19" );
