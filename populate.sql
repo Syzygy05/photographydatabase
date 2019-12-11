8d6e93d23e2a896e34237ca57d9cedd54718a5a1
Sid Patel <sidd0123@hotmail.com>
2019-12-10 19:49:35 -0500

    no changes made

--- populate.sql
+++ populate.sql
@@ -1,24 +1,24 @@
 
-INSERT INTO Customer ( LoginName, Password, CName, CType, CardNo, BillingAddress, Str1, City, State, Zip )
-       VALUES ( 'jimmy3', '123', 'James', 'Blunt', 'Mastercard', 1111111111111111, '23 willow st, Newark, NJ, 07101', 'Willow', 'Newark', 'NJ', 07101 ) 
+INSERT INTO Customer ( LoginName, Password, CName, CType, BillingAddress, Str1, City, State, Zip )
+       VALUES ( 'jimmy3', '123', 'James Blunt', 'Mastercard', '23 willow st, Newark, NJ, 07101', 'Willow', 'Newark', 'NJ', 07101 )
 
-INSERT INTO Customer ( LoginName, Password, CName, CType, CardNo, BillingAddress, Str1, City, State, Zip )$
-       VALUES ( 'johnny12', '321', 'Johnny', 'Bravo', 'Mastercard', 12222222222222, '23 willow st, Newark, NJ, 07101', 'Willow', 'Newark', 'NJ', 07101 ) 
+INSERT INTO Customer ( LoginName, Password, CName, CType, BillingAddress, Str1, City, State, Zip )$
+       VALUES ( 'johnny12', '321', 'Johnny Bravo', 'Mastercard', '23 willow st, Newark, NJ, 07101', 'Willow', 'Newark', 'NJ', 07101 )
 
-INSERT INTO Customer ( LoginName, Password, CName, CType, CardNo, BillingAddress, Str1, City, State, Zip )
-       VALUES ( 'annie32', '0101', 'Annie', 'May', 'Visa', 133333333333333, '23 willow st, Newark, NJ, 07101', 'Willow', 'Newark', 'NJ', 07101 ) 
+INSERT INTO Customer ( LoginName, Password, CName, CType, BillingAddress, Str1, City, State, Zip )
+       VALUES ( 'annie32', '0101', 'Annie May', 'Visa', '23 willow st, Newark, NJ, 07101', 'Willow', 'Newark', 'NJ', 07101 )
 
-INSERT INTO Photographer ( 
+INSERT INTO Photographer (
 	PName, PBDate, PBio, PAddress, PNationality ) VALUES (
 	'Jeff', 11-2-1998, 'cool guy', '11, the middle of nowhere', 'brazillian' );
 
 
-INSERT INTO Photographer ( 
+INSERT INTO Photographer (
 	PName, PBDate, PBio, PAddress, PNationality ) VALUES (
 	'Sally', 6-2-1983, 'nice girl', 'Valhalla', 'French' );
 
 
-INSERT INTO Photographer ( 
+INSERT INTO Photographer (
 	PName, PBDate, PBio, PAddress, PNationality ) VALUES (
 	'Martin', 7-5-1975, 'cooler than jeff', '32 wywietrnzik avenue', 'Egyptian' );
 
@@ -48,10 +48,3 @@ INSERT INTO Photo (
 INSERT INTO Photo (
 	PhotoID, Speed, Film FStop, Color, Resolution, Price, `Date`, PName, PBDate ) Values (
 	30, 12, 13.2, 'yes', '640x480', 65.50, 4-6-1999, 'Tommy', '12-19-1951' );
-
-
-
-
-
-
- 
